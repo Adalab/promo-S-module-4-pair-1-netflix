0677c051b1063f0464ec01512e3007550b2528a8
Virlucero <virlucero@gmail.com>
2023-04-04 11:34:00 +0200

    add new function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2/promise");
 
 // create and config server
 const server = express();
@@ -11,6 +12,57 @@ const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
-server.get("/"), function (req,res) {
-  res.
+server.get("/"),
+  function (req, res) {
+    res.send({
+      success: true,
+      movies: mysql,
+    });
+  };
+
+let connection;
+
+mysql
+  .createConnection({
+    host: "localhost",
+    database: "Netflix",
+    user: "root",
+    password: "Lucas020813.",
+  })
+  .then((conn) => {
+    connection = conn;
+    connection
+      .connect()
+      .then(() => {
+        console.log(
+          `Conexión establecida con la base de datos (identificador=${connection.threadId})`
+        );
+      })
+      .catch((err) => {
+        console.error("Error de conexion: " + err.stack);
+      });
+  })
+  .catch((err) => {
+    console.error("Error de configuración: " + err.stack);
+  });
+
+// routas de express
+server.get("/movies", (req, res) => {
+  let sql = "SELECT * FROM movies";
+  connection
+    .query(sql)
+    .then(([results, fields]) => {
+      console.log("Información recuperada:");
+      results.forEach((result) => {
+        console.log(result);
+      });
+
+      res.json({
+        success: true,
+        movies: results,
+      });
+    })
+    .catch((err) => {
+      throw err;
+    });
 });
