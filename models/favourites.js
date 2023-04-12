ca77b2c9c98b57376cb8de7e05be33c87df56c6f
Virlucero <virlucero@gmail.com>
2023-04-12 11:38:11 +0200

    add docker

---
+++ models/favourites.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const favoriteSchema = new Schema(
+  {
+    idUser: { type: Schema.Types.ObjectId, ref: "users" },
+    idMovie: { type: Schema.Types.ObjectId, ref: "movies" },
+    score: Integer,
+  },
+  { collection: "favorite" }
+);
+const Favorite = mongoose.model("favorite", favoriteSchema);
+module.exports = Favorite;
+
+app.post("/favorites-add", (req, res) => {
+  //código del endpoint
+});
